2c8c4de0a44ea42887056e3348cb57e6ca072184
Palm <nipalm@f45c89acd433.ant.amazon.com>
2020-04-03 12:01:44 +0200

    Configure sign up info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import logo from "./logo.svg";
+import "./App.css";
+import "@aws-amplify/ui/dist/style.css"; // Styles for login component
+
+import { withAuthenticator } from "aws-amplify-react";
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
@@ -23,4 +30,10 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, {
+  usernameAttributes: "email",
+  includeGreetings: true, // This adds the header, letting you sign out.
+  signUpConfig: {
+    hiddenDefaults: ["phone_number"]
+  }
+});
